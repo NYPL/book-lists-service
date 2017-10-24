a067197b5b41bb1e58b2c9aa41f717af3e9bf5b1
Paul Beaudoin <contact@paulbeaudoin.com>
2017-10-24 15:42:53 -0400

    Add integration tests, custom errors, logging

--- test/lambda.test.js
+++ test/lambda.test.js
@@ -2,21 +2,61 @@ const LambdaTester = require('lambda-tester')
 
 const handler = require('../index').handler
 
+// Express opens a bunch of /tmp/server* sockets during tests, so make sure they're closed on exit:
+const exitHandler = require('../index').exitHandler
+process.on('exit', exitHandler.bind(null, { cleanup: true }))
+process.on('SIGINT', exitHandler.bind(null, { exit: true })) // ctrl+c event
+process.on('SIGTSTP', exitHandler.bind(null, { exit: true })) // ctrl+v event
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
+
 describe('Lambda index handler', function () {
-  it('should respond with if 200', function () {
+  it('should respond with 200 if path valid', function () {
     return LambdaTester(handler)
       .event({
         httpMethod: 'GET',
-        pathParameters: {
-          type: 'staff-picks',
-          date: '2017-01'
-        }
+        path: '/api/v0.1/book-lists/staff-picks/2017-01'
       })
       .expectResult((result) => {
         expect(result.statusCode).to.equal(200)
         let body = JSON.parse(result.body)
         expect(body).to.be.a('object')
         expect(body.slug).to.equal('staff-picks/2017-01')
+        return true
+      })
+  })
+
+  it('should respond with 404 if path points to non-existant resource', function () {
+    return LambdaTester(handler)
+      .event({
+        httpMethod: 'GET',
+        path: '/api/v0.1/book-lists/staff-picks/2017-13-does-not-exist'
+      })
+      .expectResult((result) => {
+        expect(result.statusCode).to.equal(404)
+        let body = JSON.parse(result.body)
+        expect(body).to.be.a('object')
+      })
+  })
+
+  it('should respond with 200 if POST valid', function () {
+    return LambdaTester(handler)
+      .event({
+        httpMethod: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        path: '/api/v0.1/book-lists',
+        body: JSON.stringify({
+          type: 'staff-picks',
+          date: '2018-06',
+          picks: []
+        })
+      })
+      .expectResult((result) => {
+        expect(result.statusCode).to.equal(200)
+        let body = JSON.parse(result.body)
+        expect(body).to.be.a('object')
+        expect(body.slug).to.equal('staff-picks/2018-06')
       })
   })
 })
--- test/test-helper.js
+++ test/test-helper.js
@@ -2,13 +2,28 @@ const path = require('path')
 const fs = require('fs')
 const AWS = require('aws-sdk-mock')
 
+/**
+ * Mocked S3 error: NoSuchKey
+ */
+class NoSuchKeyError extends Error {
+  constructor (message) {
+    super()
+    this.code = 'NoSuchKey'
+  }
+}
+
 before(function () {
   AWS.mock('S3', 'upload', function (params, options, callback) {
     callback(null, { key: params.Key })
   })
   AWS.mock('S3', 'getObject', function (params, callback) {
-    let content = fs.readFileSync(path.join('./test/data', params.Key))
-    callback(null, { Body: new Buffer(content) })
+    try {
+      let content = fs.readFileSync(path.join('./test/data', params.Key))
+      callback(null, { Body: new Buffer(content) })
+    } catch (e) {
+      // Treat missing fixture as 404
+      callback(new NoSuchKeyError())
+    }
   })
 })
 
